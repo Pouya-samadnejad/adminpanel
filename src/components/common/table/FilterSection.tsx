c5a016a240b794edcaf1b1c4657779af6c5c17cc
Pouya <pouyasamadnejad@gmail.com>
2025-05-17 17:31:08 +0330

    button got modified

--- src/components/common/table/FilterSection.tsx
+++ src/components/common/table/FilterSection.tsx
@@ -19,14 +19,21 @@ const FilterSection: React.FC = () => {
     <>
       <button
         onClick={showLoading}
-        className="rounded-xl bg-gray-200 py-2.5 px-5 flex items-center justify-center"
+        className="rounded-xl bg-gray-200 py-2.5 px-5 flex items-center justify-center  hover:bg-gray-300 transition-all duration-300 cursor-pointer"
       >
         <i className="fal fa-filter text-gray-500 text-xl"></i>
       </button>
 
       <Modal
         title={<p>Loading Modal</p>}
-        footer={<button onClick={showLoading}>Reload</button>}
+        footer={
+          <button
+            onClick={showLoading}
+            className="rounded-xl bg-blue-400 py-2.5 px-5 flex items-center justify-center  hover:bg-blue-500 transition-all duration-300 cursor-pointer"
+          >
+            Reload
+          </button>
+        }
         loading={loading}
         open={open}
         onCancel={() => setOpen(false)}

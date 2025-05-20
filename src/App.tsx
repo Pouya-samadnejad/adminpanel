3a173d7b2a5a6dc31817d77d530cf1bc9aa0730e
Pouya <pouyasamadnejad@gmail.com>
2025-05-20 13:22:37 +0330

    "The form has been modified."

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { RouterProvider } from "react-router-dom";
+import { Form, RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 import { useEffect } from "react";
 import { getCurrentUser } from "./services/user";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, Input } from "antd";
 
 const App: React.FC = () => {
   useEffect(() => {
@@ -10,12 +10,16 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <ConfigProvider direction="rtl" locale={{ locale: "fa_IR" }}>
-      <div
-        style={{
-          fontFamily: "IRANYekanXFaNum, Tahoma, Arial, sans-serif ",
-        }}
-      >
+    <ConfigProvider
+      direction="rtl"
+      locale={{ locale: "fa_IR" }}
+      theme={{
+        token: {
+          fontFamily: "IRANYekanXFaNum, sans-serif",
+        },
+      }}
+    >
+      <div>
         <RouterProvider router={routes} />
       </div>
     </ConfigProvider>
--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Button, Form, Input, DatePicker, Radio, Upload } from "antd";
+import React, { useState } from "react";
+import { Form, Input, DatePicker, Radio, Upload, Drawer, Button } from "antd";
 import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs";
 
 const UserForm: React.FC = () => {
   const [form] = Form.useForm();
@@ -17,8 +18,28 @@ const UserForm: React.FC = () => {
     return e?.fileList;
   };
 
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   const variant = Form.useWatch("variant", form);
-  const navigate = useNavigate;
+  const navigate = useNavigate();
+  const beforeUpload = (file: File) => {
+    const maxSize = 320 * 1024; // 320KB
+    if (file.size > maxSize) {
+      message.error(
+        `${file.name} بیش از 320 کیلوبایت است و نمی‌تواند آپلود شود.`
+      );
+      return false; // جلوگیری از آپلود فایل
+    }
+    return true;
+  };
 
   return (
     <div>
@@ -30,26 +51,40 @@ const UserForm: React.FC = () => {
         layout="vertical"
         className="grid grid-cols-4 gap-x-2.5 my-2"
         variant={variant || "filled"}
+        noValidate
       >
         <Form.Item
           name="nationalIdCode"
           label="کد ملی"
           rules={[
             { required: true, message: "لطفا کد ملی را وارد کنید" },
+            { len: 10, message: "کد ملی باید دقیقا 10 رقم باشد" },
             {
-              message: "کد ملی باید دقیقا 10 رقم عددی باشد",
-              pattern: /^\d{10}$/,
+              pattern: /^\d+$/,
+              message: "فقط اعداد مجاز هستند",
             },
           ]}
         >
-          <Input maxLength={10} showCount />
+          <Input
+            minLength={10}
+            maxLength={10}
+            inputMode="numeric"
+            pattern="[0-9]*"
+            showCount
+            onBeforeInput={(e) => {
+              if (!/^\d*$/.test(e.data)) {
+                e.preventDefault();
+              }
+            }}
+            size="large"
+          />
         </Form.Item>
         <Form.Item
           name="firstName"
           label="نام"
           rules={[{ required: true, message: "لطفا نام خود را وارد کنید" }]}
         >
-          <Input />
+          <Input allowClear size="large" />
         </Form.Item>
 
         <Form.Item
@@ -59,38 +94,71 @@ const UserForm: React.FC = () => {
             { required: true, message: "لطفا نام خانوادگی را وارد کنید" },
           ]}
         >
-          <Input />
+          <Input allowClear size="large" />
         </Form.Item>
         <Form.Item
           name="fatherName"
           label="نام پدر"
           rules={[{ required: true, message: "لطفا نام پدر را وارد کنید" }]}
         >
-          <Input />
+          <Input allowClear size="large" />
         </Form.Item>
 
         <Form.Item
-          name="mobile"
-          label="موبایل"
-          rules={[{ required: true, message: "لطفا موبایل خود را وارد کنید" }]}
+          name="phone"
+          label="شماره تلفن"
+          rules={[
+            { required: true, message: "لطفا شماره تلفن خود را وارد کنید" },
+            {
+              pattern: /^[0-9]{11}$/,
+              message: "شماره تلفن باید شامل 11 رقم باشد",
+            },
+            {
+              pattern: /^(0|0098|\+98)?9(0[1-5]|[1 3]\d|2[0-2]|98)\d{7}$/,
+              message: "شماره تلفن وارد شده معتبر نیست",
+            },
+          ]}
         >
-          <Input maxLength={11} type="tel" />
+          <Input
+            maxLength={11}
+            inputMode="numeric"
+            pattern="[0-9]*"
+            size="large"
+            onBeforeInput={(e) => {
+              if (!/^\d*$/.test(e.data)) {
+                e.preventDefault();
+              }
+            }}
+            placeholder="مثال: 09123456789"
+          />
         </Form.Item>
 
         <Form.Item
           name="birthDate"
           label="تاریخ تولد"
           rules={[{ required: true, message: "لطفا تاریخ تولد را وارد کنید" }]}
+          getValueFromEvent={(e: any) => e?.toISOString()}
+          getValueProps={(value: any) => ({
+            value: value ? dayjs(value) : undefined,
+          })}
         >
-          <DatePicker className="w-full" />
+          <DatePicker
+            className="w-full"
+            size="large"
+            format="YYYY-MM-DD"
+            placeholder="تاریخ تولد خود را وارد کنید"
+          />
         </Form.Item>
 
         <Form.Item
           name="email"
           label="ایمیل"
-          rules={[{ required: true, message: "لطفا ایمیل خود را وارد کنید" }]}
+          rules={[
+            { required: true, message: "لطفا ایمیل خود را وارد کنید" },
+            { type: "email", message: "ایمیل وارد شده معتبر نیست" },
+          ]}
         >
-          <Input type="email" className="w-full" />
+          <Input type="email" className="w-full" size="large" />
         </Form.Item>
         <Form.Item
           name="gender"
@@ -122,6 +190,69 @@ const UserForm: React.FC = () => {
               { value: 2, label: "LDAP" },
             ]}
           />
+          <div className="my-8 flex gap-1.5 items-center">
+            <h3>سمت‌ها</h3>
+            <button
+              className="bg-teal-600 text-white px-1 rounded-md"
+              onClick={showDrawer}
+              type="button"
+            >
+              راهنما
+            </button>
+            <Drawer
+              width="30%"
+              title="راهنما"
+              closable={{ "aria-label": "Close Button" }}
+              closable={false}
+              open={open}
+              placement="left"
+              extra={
+                <Button
+                  type="text"
+                  onClick={onClose}
+                  icon={<i className="fal fa-x"></i>} // آیکون دکمه بسته شدن
+                  style={{
+                    marginLeft: "auto",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                />
+              }
+            >
+              <section className="p-4">
+                <li className="mt-2 mb-5 w-full flex flex-col justify-center items-center text-teal-700">
+                  <span className="relative inline-block w-fit">
+                    <i className="fal fa-layer-group text-6xl"></i>
+                    <i className="fal fa-user-vneck text-3xl absolute -bottom-1 -right-1 bg-white rounded-full p-0.5"></i>
+                  </span>
+
+                  <strong className="text-info">سمــت کـــــاربر</strong>
+                </li>
+                <h2 className="text-xl font-bold mb-4">سمت در چارت سازمانی</h2>
+                <ul className="list-disc pr-6 space-y-2 text-right">
+                  <li>
+                    منظور از سمت، موقعیت کاربر در چارت سازمانی است، مانند
+                    مدیرکل، مدیرعامل، کارشناس، معاون مالی و ...
+                  </li>
+                  <li>
+                    سمت‌ها در بخش مدیریت درختواره قابل تعریف و ویرایش هستند.
+                  </li>
+                  <li>
+                    با تعیین سمت کاربر، می‌توانید موقعیت او را در گراف و درخت
+                    سازمانی مشاهده کنید.
+                  </li>
+                  <li>
+                    با ثبت سمت، امکان دسترسی به سامانه بر اساس نقش کاربر فراهم
+                    می‌شود.
+                  </li>
+                  <li>
+                    در قسمت مدیریت سمت‌ها، می‌توانید کاربر مورد نظر را به سمت
+                    دلخواه متصل کنید.
+                  </li>
+                </ul>
+              </section>
+            </Drawer>
+          </div>
         </Form.Item>
         <div className="flex items-center gap-3 col-span-4 mb-10">
           <h2 className="font-bold">تصاویر</h2>
@@ -133,13 +264,14 @@ const UserForm: React.FC = () => {
           valuePropName="fileList"
           getValueFromEvent={normFile}
           className="col-span-4"
-          rules={[{ required: true, message: "لطفا یک فایل آپلود کنید" }]}
+          rules={[{ required: false, message: "لطفا یک فایل آپلود کنید" }]}
         >
           <Upload.Dragger
+            beforeUpload={beforeUpload}
             name="files"
-            action="/upload.do"
+            showUploadList={false}
             accept=".png,.jpg"
-            maxCount={1}
+            maxCount={3}
             className="w-full "
             style={{ width: "100%" }}
           >
@@ -153,16 +285,21 @@ const UserForm: React.FC = () => {
             <p className="ant-upload-hint">حداکثر حجم فایل 320 KB</p>
           </Upload.Dragger>
         </Form.Item>
-        <div className="flex items-center justify-end col-span-4 gap-2">
+        <div className="flex items-center justify-end col-span-4 gap-2 fixed bottom-0 left-0 p-4 bg-white w-full shadow-2xl">
           <button
-            className="bg-gray-200 text-gray-600 px-4 py-3 text-xl rounded-xl hover:bg-gray-300 transition-all duration-200"
+            className="bg-gray-200 text-gray-600 px-4 py-3 rounded-[12px] hover:bg-gray-300 transition-all duration-200 cursor-pointer"
             onClick={() => {
               navigate(-1);
             }}
           >
             انصراف
           </button>
-          <button>ثبت و ذخیره</button>
+          <button
+            onClick={onFinish}
+            className="bg-sky-500 text-white px-12 py-3  rounded-[12px] hover:bg-sky-600 transition-all duration-200 cursor-pointer"
+          >
+            ثبت و ذخیره
+          </button>
         </div>
       </Form>
     </div>
